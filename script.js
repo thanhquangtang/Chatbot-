b7e7fad45269d47bf0a8c21aad6a5c5377b71178
thanhquangtang <thanhquangtang298@gmail.com>
2023-09-22 00:24:15 +0700

    chatbot

---
+++ script.js
@@ -0,0 +1,135 @@
+var OPENAI_API_KEY = "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";
+var bTextToSpeechSupported = false;
+var bSpeechInProgress = false;
+var oSpeechRecognizer = null
+var oSpeechSynthesisUtterance = null;
+var oVoices = null;
+
+function OnLoad() {
+    if ("webkitSpeechRecognition" in window) {
+    } else 
+        lblSpeak.style.display = "none";
+
+    if ('speechSynthesis' in window) {
+        bTextToSpeechSupported = true;
+
+        speechSynthesis.onvoiceschanged = function () {
+            oVoices = window.speechSynthesis.getVoices();
+            for (var i = 0; i < oVoices.length; i++) {
+                selVoices[selVoices.length] = new Option(oVoices[i].name, i);
+            }
+        };
+    }
+}
+
+function ChangeLang(o) {
+    if (oSpeechRecognizer) {
+        oSpeechRecognizer.lang = selLang.value;
+        //SpeechToText()
+    }
+}
+
+function Send() {
+
+    var sQuestion = txtMsg.value;
+    if (sQuestion == "") {
+        alert("Type in your question!");
+        txtMsg.focus();
+        return;
+    }
+
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", "https://api.openai.com/v1/completions");
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {
+            var oJson = {}
+            if (txtOutput.value != "") txtOutput.value += "\n";
+
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                txtOutput.value += "Error: " + ex.message
+            }
+
+            if (oJson.error && oJson.error.message) {
+                txtOutput.value += "Error: " + oJson.error.message;
+            } else if (oJson.choices && oJson.choices[0].text) {
+                var s = oJson.choices[0].text;
+
+                if (selLang.value != "en-US") {
+                    var a = s.split("?\n");
+                    if (a.length == 2) {
+                        s = a[1];
+                    }
+                }
+
+                if (s == "") s = "No response";
+                txtOutput.value += "Chat GPT: " + s;
+                TextToSpeech(s);
+            }            
+        }
+    };
+
+    var sModel = selModel.value;// "text-davinci-003";
+    var iMaxTokens = 2048;
+    var sUserId = "1";
+    var dTemperature = 0.5;    
+
+    var data = {
+        model: sModel,
+        prompt: sQuestion,
+        max_tokens: iMaxTokens,
+        user: sUserId,
+        temperature:  dTemperature,
+        frequency_penalty: 0.0, //Number between -2.0 and 2.0  
+        presence_penalty: 0.0,  //Number between -2.0 and 2.0.                           
+        stop: ["#", ";"]        
+    }
+
+    oHttp.send(JSON.stringify(data));
+
+    if (txtOutput.value != "") txtOutput.value += "\n";
+    txtOutput.value += "Me: " + sQuestion;
+    txtMsg.value = "";
+}
+
+function TextToSpeech(s) {
+    if (bTextToSpeechSupported == false) return;
+    if (chkMute.checked) return;
+
+    oSpeechSynthesisUtterance = new SpeechSynthesisUtterance();
+
+    if (oVoices) {
+        var sVoice = selVoices.value;
+        if (sVoice != "") {
+            oSpeechSynthesisUtterance.voice = oVoices[parseInt(sVoice)];
+        }        
+    }    
+
+    oSpeechSynthesisUtterance.onend = function () {
+        if (oSpeechRecognizer && chkSpeak.checked) {
+            oSpeechRecognizer.start();
+        }
+    }
+
+    if (oSpeechRecognizer && chkSpeak.checked) {
+        oSpeechRecognizer.stop();
+    }
+
+    oSpeechSynthesisUtterance.lang = selLang.value;
+    oSpeechSynthesisUtterance.text = s;
+    window.speechSynthesis.speak(oSpeechSynthesisUtterance);
+}
+
+function Mute(b) {
+    if (b) {
+        selVoices.style.display = "none";
+    } else {
+        selVoices.style.display = "";
+    }
+}
+
